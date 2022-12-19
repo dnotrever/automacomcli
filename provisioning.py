8215c88c2959478325f44a11141e4ddede21f85f
dnotrever <rosa.everton@hotmail.com>
2022-12-19 19:51:41 -0300

    implements some changes and fixes bugs

--- provisioning.py
+++ provisioning.py
@@ -21,11 +21,12 @@ def customer_id():
 
     id_list.close()
 
+
 def customer_infos(customer_id):
 
     from unicodedata import normalize
     import re
-    import config
+    import config, helpers
 
     for _, row in config.registrations.iterrows():
 
@@ -51,159 +52,185 @@ def customer_infos(customer_id):
                     customer_name.append(arr_name[x])
 
             block = ''.join(re.findall('[0-9]+', block))
+            
+            if len(block) == 1: block = '0' + block
+            
             apt = ''.join(re.findall('[0-9]+', apt))
 
-            customer_infos = f'{customer_name[0]}-{customer_name[1]}-bl{block}-apto{apt}'
+            infos = f'{customer_name[0]}-{customer_name[1]}-bl{block}-apto{apt}'
 
-            return [customer_infos, condominium, block]
+            return [name, infos, condominium, block]
 
-def provis_customer_datacom(customer_id):
 
-    import telnetlib, time, infra_configs
+def provis_customer_datacom():
+
+    import telnetlib, time, os, infra_configs
+    from datetime import date, datetime
+    
+    customer_id = input('\nID: ')
+
+    # # - - - - - - - - - - - - - - - - - - - - 
+    
+    name, infos, condominium, block = customer_infos(customer_id)
+
+    ip_olt = infra_configs.discover_olt(condominium)
+    
+    print('\nolt: ' + ip_olt)
+    
+    print('cliente: ' + infos)
 
     # # - - - - - - - - - - - - - - - - - - - - 
 
-    infos, condominium, block = customer_infos(customer_id)
+    try:
+        telnet = telnetlib.Telnet(ip_olt)
+    except:
+        print('Erro ao se conectar!')
 
-    ip_olt = infra_configs.discover_olt(condominium)
+    user = b'erosa\n'
+    password = b'123@mudar\n' 
 
-    print('\nolt: ' + ip_olt)
+    telnet.read_until(b': ')
+    telnet.write(user)
 
-    print('cliente: ' + infos)
+    telnet.read_until(b': ')
+    telnet.write(password)
 
-    # # # - - - - - - - - - - - - - - - - - - - - 
+    telnet.read_until(b'# ')
+    telnet.write(b'config\n')
 
-    # try:
-    #     telnet = telnetlib.Telnet(ip_olt)
-    # except:
-    #     print('Erro ao se conectar!')
+    # # - - - - - - - - - - - - - - - - - - - - 
 
-    # user = b'erosa\n'
-    # password = b'123@mudar\n' 
+    telnet.read_until(b'# ')
 
-    # telnet.read_until(b': ')
-    # telnet.write(user)
+    telnet.write(b'do show interface gpon discovered-onus\n')
 
-    # telnet.read_until(b': ')
-    # telnet.write(password)
-
-    # telnet.read_until(b'# ')
-    # telnet.write(b'config\n')
-
-    # # # - - - - - - - - - - - - - - - - - - - - 
-
-    # telnet.read_until(b'# ')
-
-    # telnet.write(b'do show interface gpon discovered-onus\n')
-
-    # telnet.read_until(b'\n')
+    telnet.read_until(b'\n')
     
-    # time.sleep(2)
+    time.sleep(2)
 
-    # onu_infos = telnet.read_very_eager().decode('utf-8').splitlines()
+    onu_infos = telnet.read_very_eager().decode('utf-8').splitlines()
 
-    # if '% No entries found.' in onu_infos:
-    #     print('Sem ONU!')
-    # else:
-    #     onu_mac = onu_infos[3].split()[1]
-    #     onu_gpon = onu_infos[3].split(' ')[7]
+    if '% No entries found.' in onu_infos:
+        print('Sem ONU!')
+        return
+    else:
+        onu_mac = onu_infos[3].split()[1]
+        print('mac: ' + onu_mac)
+        onu_gpon = onu_infos[3].split(' ')[7]
+        print('gpon: ' + onu_gpon)
 
-    # print('mac: ' + onu_mac)
-    # print('gpon: ' + onu_gpon)
+    vlan = infra_configs.discover_vlan(condominium, onu_gpon, block)
 
-    # vlan = infra_configs.discover_vlan(condominium, onu_gpon, block)
+    print('vlan: ' + vlan)
 
-    # print('vlan: ' + vlan)
+    # # - - - - - - - - - - - - - - - - - - - - 
 
-    # # # - - - - - - - - - - - - - - - - - - - - 
+    telnet.write(f'do show running-config interface gpon {onu_gpon} | nomore'.encode('ascii') + b'\n')
 
-    # telnet.write(f'do show running-config interface gpon {onu_gpon} | nomore'.encode('ascii') + b'\n')
+    telnet.read_until(b'\n')
+    time.sleep(5)
 
-    # telnet.read_until(b'\n')
-    # time.sleep(4)
+    onus = telnet.read_very_eager().splitlines()
 
-    # onus = telnet.read_very_eager().splitlines()
+    onus_arr = []
 
-    # onus_arr = []
+    for onu in onus:
 
-    # for onu in onus:
+        onu_str = onu.decode('utf-8')
 
-    #     onu_str = onu.decode('utf-8')
+        if 'onu' in onu_str:
+            onus_arr.append(onu_str)
 
-    #     if 'onu' in onu_str:
-    #         onus_arr.append(onu_str)
+    last_onu = str(onus_arr[len(onus_arr)-1])
 
-    # last_onu = str(onus_arr[len(onus_arr)-1])
+    curr_onu = int(last_onu.split(' ')[2]) + 1
 
-    # curr_onu = int(last_onu.split(' ')[2]) + 1
+    print('onu:', curr_onu)
 
-    # print('\nonu:', curr_onu)
+    # # - - - - - - - - - - - - - - - - - - - - 
 
-    # # # - - - - - - - - - - - - - - - - - - - - 
+    telnet.write(f'interface gpon {onu_gpon} onu {curr_onu}'.encode('ascii') + b'\n')
 
-    # telnet.write(f'interface gpon {onu_gpon} onu {curr_onu}'.encode('ascii') + b'\n')
+    telnet.read_until(b'\n')
 
-    # telnet.read_until(b'\n')
+    telnet.write(f'serial-number {onu_mac}'.encode('ascii') + b'\n')
 
-    # telnet.write(f'serial-number {onu_mac}'.encode('ascii') + b'\n')
+    telnet.read_until(b'# ')
 
-    # telnet.read_until(b'# ')
+    telnet.write(f'name {infos}'.encode('ascii') + b'\n')
 
-    # telnet.write(f'name {customer_infos}'.encode('ascii') + b'\n')
+    telnet.read_until(b'# ')
 
-    # telnet.read_until(b'# ')
+    telnet.write(f'line-profile DEFAULT-LINE\nethernet 1\nnegotiation\nno shutdown\nnative vlan vlan-id {vlan}\nnative vlan cos 0'.encode('ascii') + b'\n')
 
-    # telnet.write(f'line-profile DEFAULT-LINE\nethernet 1\nnegotiation\nno shutdown\nnative vlan vlan-id {vlan}\nnative vlan cos 0'.encode('ascii') + b'\n')
+    telnet.read_until(b'# ')
 
-    # telnet.read_until(b'# ')
+    telnet.write(b'commit\n')
 
-    # telnet.write(b'commit\n')
+    telnet.read_until(b'# ')
 
-    # telnet.read_until(b'# ')
+    telnet.write(b'top\n')
 
-    # telnet.write(b'top\n')
+    # # - - - - - - - - - - - - - - - - - - - - 
 
-    # # # - - - - - - - - - - - - - - - - - - - - 
+    telnet.read_until(b'# ')
 
-    # telnet.read_until(b'# ')
+    telnet.write(b'do show running-config | include service-port | nomore\n')
 
-    # telnet.write(b'do show running-config | include service-port | nomore\n')
+    telnet.read_until(b'# ')
+    time.sleep(45)
 
-    # telnet.read_until(b'# ')
-    # time.sleep(35)
+    services = telnet.read_very_eager().splitlines()
 
-    # services = telnet.read_very_eager().splitlines()
+    services_arr = []
 
-    # services_arr = []
+    for service in services:
 
-    # for service in services:
+        service_str = service.decode('utf-8')
 
-    #     service_str = service.decode('utf-8')
+        services_arr.append(service_str)
 
-    #     services_arr.append(service_str)
+    last_service = str(services_arr[len(services_arr)-2])
 
-    # last_service = str(services_arr[len(services_arr)-2])
+    curr_service = int(last_service.split(' ')[1]) + 1
 
-    # curr_service = int(last_service.split(' ')[1]) + 1
+    print('service port:', curr_service)
 
-    # print('\nService Port:', curr_service)
+    # # - - - - - - - - - - - - - - - - - - - - 
 
-    # # # - - - - - - - - - - - - - - - - - - - - 
+    telnet.write(f'service-port {curr_service} gpon {onu_gpon} onu {curr_onu} gem 1 match vlan vlan-id {vlan} action vlan replace vlan-id {vlan}'.encode('ascii') + b'\n')
 
-    # telnet.write(f'service-port {curr_service} gpon {onu_gpon} onu {curr_onu} gem 1 match vlan vlan-id {vlan} action vlan replace vlan-id {vlan}'.encode('ascii') + b'\n')
+    telnet.read_until(b'# ')
 
-    # time.sleep(2)
+    telnet.write(b'commit\n')
+    
+    time.sleep(3)
 
-    # telnet.read_until(b'# ')
+    # # - - - - - - - - - - - - - - - - - - - - 
+    
+    today = date.today()
+    now_date = str(datetime.now()).split(' ')[1]
+    now_time = now_date.split('.')[0]
 
-    # telnet.write(b'commit\n')
+    provis_logs = open(f'../Provising_Logs_{today}.txt', 'w')
+    provis_logs.write(f'\n[{now_time}]\n{name} ({condominium})\nMAC: {onu_mac}\nGPON: {onu_gpon}\nONU: {curr_onu}\nVLAN: {vlan}\nService Port: {curr_service}\n')
+    provis_logs.close()
 
-    # # # - - - - - - - - - - - - - - - - - - - - 
+    # return f'Cliente {name} instalado com sucesso!'
 
-    # return 'Cliente instalado com sucesso!'
+    provis_customer_datacom()
+
 
 def provis_customer_huawei(customer_id):
 
     pass
 
-provis_customer_datacom('9118')
+
+# print(customer_infos('9099'))
+
+provis_customer_datacom()
+
+
+# import telnetlib
+# telnet = telnetlib.Telnet('10.72.181.59')
+# telnet.interact()
